fd0e05da820e3d0a69dfeb21c1b2660473a3a28f
Farouq Umar <ufakz99@gmail.com>
2022-08-16 21:30:21 +0100

    feat: implement models

--- cashman/index.py
+++ cashman/index.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, World"
+incomes = [
+    {'description': 'salary', 'amount': 5000}
+]
+
+
+@app.route('/incomes')
+def get_incomes():
+    return jsonify(incomes)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+    incomes.append(request.get_json())
+    return '', 204
